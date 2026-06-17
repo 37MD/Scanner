83e67906c0b1686678b35753d43a8b7c77614cbe
37MD <150575622+37MD@users.noreply.github.com>
2026-06-17 12:46:21 +0530

    Add files via upload

---
+++ nse_ba_data.js
@@ -0,0 +1,12 @@
+const EMBEDDED_BA_DATA = {
+  "28062024": {"nifty 10 yr benchmark g-sec": 2331.82, "nifty 4-8 yr g-sec index": 2851.97, "nifty 50": 24010.6, "nifty 8-13 yr g-sec": 2632.68},
+  "29052026": {"nifty 10 yr benchmark g-sec": 2611.13, "nifty 4-8 yr g-sec index": 3253, "nifty 50": 23547.75, "nifty 8-13 yr g-sec": 2980.14},
+  "29122023": {"nifty 10 yr benchmark g-sec": 2232.79, "nifty 4-8 yr g-sec index": 2745.83, "nifty 50": 21731.4, "nifty 8-13 yr g-sec": 2522.05},
+  "30062022": {"nifty 10 yr benchmark g-sec": 1986.11, "nifty 4-8 yr g-sec index": 2455.1, "nifty 50": 15780.25, "nifty 8-13 yr g-sec": 2232.97},
+  "30062023": {"nifty 10 yr benchmark g-sec": 2170.41, "nifty 4-8 yr g-sec index": 2652.4, "nifty 50": 19189.05, "nifty 8-13 yr g-sec": 2447.75},
+  "30062025": {"nifty 10 yr benchmark g-sec": 2595.75, "nifty 4-8 yr g-sec index": 3161.86, "nifty 50": 25517.05, "nifty 8-13 yr g-sec": 2931.04},
+  "30122022": {"nifty 10 yr benchmark g-sec": 2065.94, "nifty 4-8 yr g-sec index": 2544.27, "nifty 50": 18105.3, "nifty 8-13 yr g-sec": 2332.94},
+  "31122024": {"nifty 10 yr benchmark g-sec": 2445.95, "nifty 4-8 yr g-sec index": 2983.97, "nifty 50": 23644.8, "nifty 8-13 yr g-sec": 2766.28},
+  "31122025": {"nifty 10 yr benchmark g-sec": 2613.56, "nifty 4-8 yr g-sec index": 3222.34, "nifty 50": 26129.6, "nifty 8-13 yr g-sec": 2969.31},
+};
+
